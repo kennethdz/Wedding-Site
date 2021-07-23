5130ca894645f3bd9cf652ce6ca7866fd89e48ee
kennethdz <72673496+kennethdz@users.noreply.github.com>
2021-07-23 00:18:30 -0700

    v0.1 of website
    Nav bar not consistent
    Nav bar does not go away when pressed
    RSVP accept button does not uncheck when pressing decline
    Not all Pictures are same size, so photo slideshow is inconsistent

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
-const YOUR_RAPID_API_KEY_GOES_HERE = "a6s5dfi8as5df98a5sd8f5a964sf98asd5f08asd75f9a67s4d";
 
+let slideIndex = 1;
+
+// resize Nav Bar based on size (mobile)
 function resizeNavBar() {
     let x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
@@ -9,30 +11,41 @@ function resizeNavBar() {
     }
   }
 
+  // show Home tab
 function showHome() {
     changeTab("home-tab");
     changeContent("home-content");
 }
 
+  // show Mimi tab
 function showMimi() {
     changeTab("mimi-tab");
     changeContent("mimi-content");
 }
 
-function showRSVP() {
-    changeTab("rsvp-tab");
-    changeContent("rsvp-content");
-}
-
+  // show Invitation tab
 function showInvitation() {
     changeTab("invitation-tab");
     changeContent("invitation-content");
 }
 
-let text = require('textbelt');
+  // show RSVP tab
+  function showRSVP() {
+    changeTab("rsvp-tab");
+    changeContent("rsvp-content");
+}
+  // show RSVP tab
+  function showDetails() {
+    changeTab("details-tab");
+    changeContent("details-content");
+}
 
-
-
+  // show RSVP tab
+  function showPhotos() {
+    changeTab("photos-tab");
+    changeContent("photos-content");
+    showDivs(slideIndex);
+}
 
 // tab of tabID becomes active and the rest become inactive
 function changeTab(tabID) {
@@ -59,7 +72,7 @@ function changeContent(contentID) {
         }
     }
 }
-
+// submit text message to Kenneth's phone number based on input
 async function submitText() {
     let accept = document.getElementById("input-accept");
     let decline = document.getElementById("input-decline");
@@ -74,19 +87,12 @@ async function submitText() {
     }
     console.log(uri);
     let encoded = encodeURIComponent(uri);
-    let phone = "19167935731"
+    let phone = "19167935731";
     let href = "sms:+" + phone + "&body=" + encoded;
     location.href= href;
-
-    // await TwilioSMS(9167935731, uri);
-    // text.send('9167935731', 'A sample text message!', undefined, function(err) {
-    //     if (err) {
-    //       console.log(err);
-    //     }
-    //   });
     
 }
-
+ // prevent RSVP checking both accept and decline
 function checkChoice() {
     console.log("clicked");
     let accept = document.getElementById("input-accept");
@@ -100,91 +106,19 @@ function checkChoice() {
 }
 
 
-async function getAccountId() {
-    const response = await fetch("https://twilio-sms.p.rapidapi.com/2010-04-01/Account", {
-        "method": "GET",
-        "headers": {
-          "x-rapidapi-host": "twilio-sms.p.rapidapi.com",
-          "x-rapidapi-key": YOUR_RAPID_API_KEY_GOES_HERE
-        }
-      });
-      const body = await response.json();
-      console.log('getAccountId', body);
-      return body.sid;
+
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
 }
-async function getAvailableNumbers(accountId) {
-    const result = await fetch("https://twilio-sms.p.rapidapi.com/2010-04-01/Accounts/" + accountId + "/AvailablePhoneNumbers/us/Local.json", {
-        "method": "GET",
-        "headers": {
-          "x-rapidapi-host": "twilio-sms.p.rapidapi.com",
-          "x-rapidapi-key": YOUR_RAPID_API_KEY_GOES_HERE
-        }
-      });
-      const body = await result.json();
-      console.log('getAvailableNumbers', body);
-      return body
-        .filter(item => item.capabilities.SMS)
-        .map(item => ({
-          number: item.phoneNumber.slice(1),
-          display: item.friendlyName,
-        }));
-}
-async function buyPhoneNumber(accountId, number) { 
-    const result = await fetch("https://twilio-sms.p.rapidapi.com/2010-04-01/Accounts/" + accountId + "/IncomingPhoneNumbers.json?phoneNumber=" + number + "&phoneNumberType=local&countryCode=us", {
-        "method": "POST",
-        "headers": {
-          "x-rapidapi-host": "twilio-sms.p.rapidapi.com",
-          "x-rapidapi-key": YOUR_RAPID_API_KEY_GOES_HERE,
-          "content-type": "application/x-www-form-urlencoded"
-        },
-      });
-      const body = await result.json();
-      console.log('buyPhoneNumber', body);
-      return body.phoneNumber?.sid;
- }
-async function sendSMS(accountId, phoneId, to, msg) { 
-    to = encodeURIComponent(to);
-    msg = encodeURIComponent(msg);
-    const result = await fetch("https://twilio-sms.p.rapidapi.com/2010-04-01/Accounts/" + accountId + "/Messages.json?from=" + phoneId + "&body=" + msg + "&to=" + to, {
-      "method": "POST",
-      "headers": {
-        "x-rapidapi-host": "twilio-sms.p.rapidapi.com",
-        "x-rapidapi-key": YOUR_RAPID_API_KEY_GOES_HERE,
-        "content-type": "application/x-www-form-urlencoded"
-      },
-    });
-    const body = await result.json();
-    console.log('sendSMS', body);
-    return body.status;
-}
-async function deletePhoneNumber(accountId, phoneNumberId) { 
-    const result = await fetch("https://twilio-sms.p.rapidapi.com/2010-04-01/Accounts/" + accountId + "/IncomingPhoneNumbers/" + phoneNumberId + ".json", {
-        "method": "DELETE",
-        "headers": {
-          "x-rapidapi-host": "twilio-sms.p.rapidapi.com",
-          "x-rapidapi-key": YOUR_RAPID_API_KEY_GOES_HERE
-        }
-      });
-      const body = await result.json();
-      console.log('deletePhoneNumber', body);
-}
-async function TwilioSMS(to, msg) {
-//   event.preventDefault();
-//   const form = event.target;
-//   const to = form.querySelector('[name=to]').value;
-//   const msg = form.querySelector('[name=msg]').value;
-  const accountId = await getAccountId();
-  const numbers = await getAvailableNumbers(accountId);
-  let phone;
-  for await (const { number } of numbers) {
-    const phoneId = await buyPhoneNumber(accountId, number);
-    if (phoneId) {
-      phone = { id: phoneId, number };
-      break;
-    }
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length} ;
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
   }
-  const status = await sendSMS(accountId, phone.number, to, msg);
-  await deletePhoneNumber(accountId, phone.id);
-  alert(`Delivery status: ${status}`);
-  console.log("text sent!");
+  x[slideIndex-1].style.display = "block";
 }
